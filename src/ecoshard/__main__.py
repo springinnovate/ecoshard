8de0544f86e40b6d85be1037c53c0c5f4f514789
Rich Sharp <richpsharp@gmail.com>
2019-06-26 15:59:17 -0700

    testing valid, invalid and hashing

--- src/ecoshard/__main__.py
+++ src/ecoshard/__main__.py
@@ -1,7 +1,6 @@
 """Entry point for ecoshard."""
-import shutil
-import argparse
 import os
+import argparse
 import sys
 import logging
 import glob
@@ -12,10 +11,9 @@ LOGGER = logging.getLogger(__name__)
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format=(
-        '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
-        ' [%(funcName)s:%(lineno)d] %(message)s'),
+    format=('%(message)s'),
     stream=sys.stdout)
+logging.getLogger('ecoshard').setLevel(logging.WARN)
 LOGGER = logging.getLogger(__name__)
 
 
@@ -26,21 +24,58 @@ if __name__ == '__main__':
     parser.add_argument(
         '--hashalg', nargs=1, default='md5',
         help='hash algorithm in hashlib.algorithms_available')
+
     parser.add_argument(
-        '--rename', action='store_true',
-        help='rename the files, rather than copy new ones.')
+        '--compress', action='store_true', help='compress the raster files.')
+    parser.add_argument(
+        '--buildoverviews', action='store_true',
+        help='build overviews on the raster files.')
+    parser.add_argument(
+        '--rename', action='store_true', help=(
+            'if not compressing and hashing only, rename files rather than '
+            'copy new ones.'))
+    parser.add_argument(
+        '--interpolation_method', help=(
+            'used when building overviews, can be one of '
+            '"average|near|mode|min|max|..."'), default='near')
+    parser.add_argument(
+        '--validate', action='store_true', help=(
+            'validate the ecoshard rather than hash it'))
+
     args = parser.parse_args()
-    for file_path in args.filepath:
-        hash_val = calculate_hash(file_path, args.hashalg)
-        path_pre, path_suf = os.path.splitext(file_path)
-        target_path = '%s_%s_%s%s' % (
-            path_pre, args.hashalg, hash_val, path_suf)
-        if os.path.exists(target_path):
-            LOGGER.warn('%s already exists, not replacing')
-            continue
-        if args.rename:
-            LOGGER.info('renaming %s to %s', file_path, target_path)
-            os.rename(file_path, target_path)
-        else:
-            LOGGER.info('copying %s to %s', file_path, target_path)
-            shutil.copyfile(file_path, target_path)
+    for glob_pattern in args.filepath:
+        for file_path in glob.glob(glob_pattern):
+            working_file_path = file_path
+            if args.compress:
+                prefix, suffix = os.path.splitext(file_path)
+                compressed_filename = '%s_compressed%s' % (prefix, suffix)
+                ecoshard.compress_raster(
+                    file_path, compressed_filename,
+                    compression_algorithm='LZW', compression_predictor=2)
+                working_file_path = compressed_filename
+
+            if args.buildoverviews:
+                overview_token_path = '%s.OVERVIEWCOMPLETE' % (
+                    working_file_path)
+                ecoshard.build_overviews(
+                    working_file_path, target_token_path=overview_token_path,
+                    interpolation_method=args.interpolation_method)
+
+            if args.validate:
+                try:
+                    is_valid = ecoshard.validate(working_file_path)
+                    if is_valid:
+                        LOGGER.info('VALID ECOSHARD: %s', working_file_path)
+                    else:
+                        LOGGER.error(
+                            'got a False, but no ValueError on validate? '
+                            'that is not impobipible?')
+                except ValueError:
+                    LOGGER.error('INVALID ECOSHARD: %s', working_file_path)
+            else:
+                hash_token_path = '%s.ECOSHARDCOMPLETE' % (
+                    working_file_path)
+                ecoshard.hash_file(
+                    working_file_path, target_token_path=hash_token_path,
+                    rename=args.rename, hash_algorithm=args.hashalg,
+                    force=False)
