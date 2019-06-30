3a767e46e04840b89bb8040f2f81fe11ab1b2076
Rich Sharp <richpsharp@gmail.com>
2019-06-30 14:03:35 -0700

    building a payload

--- load_to_geoserver.py
+++ load_to_geoserver.py
@@ -1,4 +1,5 @@
 """Load rasters into Geoserver."""
+import os
 import sys
 import logging
 import argparse
@@ -30,10 +31,16 @@ def main():
     parent_path = '/mnt/disks/geoserver_data/'
     for filepath in glob.glob(
             '/mnt/disks/geoserver_data/data/cv_sld_files/*.sld'):
-        local_path = filepath[len(parent_path):]
-        LOGGER.debug(local_path)
-
-
+        style_path = filepath[len(parent_path):]
+        LOGGER.debug(style_path)
+        style_name = os.path.splitext(os.path.basename(filepath))[0]
+        payload = {
+          "style": {
+            "name": style_name,
+            "filename": style_path
+          }
+        }
+        LOGGER.debug(payload)
     #response = session.get('http://localhost:8080/geoserver/rest/workspaces/cv_coastal_points_output_md5_69641307c3c7b4c7d23faa8637e30f83/styles/rhab.json', timeout=REQUEST_TIMEOUT)
 
     #LOGGER.info(response)
