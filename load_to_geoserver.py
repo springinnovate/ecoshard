6adebc35c8d000f313b141f2e2d22f94977164c2
Rich Sharp <richpsharp@gmail.com>
2019-06-30 10:59:40 -0700

    trying the requests library

--- load_to_geoserver.py
+++ load_to_geoserver.py
@@ -1,4 +1,5 @@
 """Load rasters into Geoserver."""
+import requests
 import sys
 import logging
 import argparse
@@ -12,6 +13,8 @@ logging.basicConfig(
     stream=sys.stdout)
 LOGGER = logging.getLogger(__name__)
 
+REQUEST_TIMEOUT = 5.0
+
 
 def main():
     """Entry point."""
@@ -20,21 +23,14 @@ def main():
     parser.add_argument('password', help='password')
     args = parser.parse_args()
 
-    auth_handler = urllib.request.HTTPBasicAuthHandler()
-    auth_handler.add_password(
-        realm=None,
-        uri='http://localhost/',
-        user=args.username,
-        passwd=args.password)
+    session = requests.Session()
+    session.auth = (args.username, args.password)
 
-    opener = urllib.request.build_opener(auth_handler)
-    urllib.request.install_opener(opener)
+    response = session.get(
+        'http://localhost:8080/geoserver/rest/workspaces/cv_coastal_points_output_md5_69641307c3c7b4c7d23faa8637e30f83/styles/rhab.json',
+        timeout=REQUEST_TIMEOUT)
 
-    req = urllib.request.Request(
-        url='http://localhost:8080/geoserver/rest/workspaces/cv_coastal_points_output_md5_69641307c3c7b4c7d23faa8637e30f83/styles/rhab.json',
-        method='GET')
-    x = urllib.request.urlopen(req)
-    LOGGER.info(x.text)
+    LOGGER.info(response)
 
 
 if __name__ == '__main__':
