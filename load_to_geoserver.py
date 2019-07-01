cc701acb572b9d72dbb028d22705bc816ff70e7f
Rich Sharp <richpsharp@gmail.com>
2019-06-30 17:03:18 -0700

    fixing the subpath to the file

--- load_to_geoserver.py
+++ load_to_geoserver.py
@@ -29,20 +29,23 @@ def main():
     session = requests.Session()
     session.auth = (args.username, args.password)
 
+    path_prefix = '/mnt/disks/geoserver_data/'
     for filepath in glob.glob(
             '/mnt/disks/geoserver_data/data/*.tif'):
+        sub_path = filepath[len(path_prefix):]
         LOGGER.debug(filepath)
+        LOGGER.debug(sub_path)
         compress_match = re.match(
-            '^(.*)_compressed', os.path.basename(filepath))
+            '^(.*)_compressed', os.path.basename(sub_path))
         if compress_match:
             raster_name = compress_match.group(1)
         else:
             raster_name = compress_match = re.match(
-                '^(.*)_md5', os.path.basename(filepath)).group(1)
+                '^(.*)_md5', os.path.basename(sub_path)).group(1)
         payload = {
             "coverageStore": {
                 "name": raster_name,
-                "url": "file:%s" % filepath
+                "url": "file:%s" % sub_path
             }
         }
         url = 'http://localhost:8080/geoserver/rest/workspaces/ipbes/coveragestores'
