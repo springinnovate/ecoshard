2f06dd45c4a1edb137ca908b58cd0dfba8255993
Rich Sharp <rich@therealspring.org>
2022-02-28 09:27:25 -0800

    import nondaemonic process pool

--- src/taskgraph/__init__.py
+++ src/taskgraph/__init__.py
@@ -5,7 +5,9 @@ from .Task import Task
 from .Task import _TASKGRAPH_DATABASE_FILENAME
 from .Task import __version__
 from .Task import NonDaemonicPool
+from .Task import NonDaemonicProcessPoolExecutor
 
 __all__ = [
     '__version__', 'TaskGraph', 'Task',
-    '_TASKGRAPH_DATABASE_FILENAME', 'NonDaemonicPool']
+    '_TASKGRAPH_DATABASE_FILENAME', 'NonDaemonicPool',
+    'NonDaemonicProcessPoolExecutor']
