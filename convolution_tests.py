9007bda11f2a62a3eb5ec4b714c6e297911707a8
Rich Sharp <rich@therealspring.org>
2022-05-13 10:44:34 -0700

    building up a test script

--- convolution_tests.py
+++ convolution_tests.py
@@ -1,7 +1,10 @@
 """Tracer for running convolution tests."""
 import logging
 
+from ecoshard import geoprocessing
 from osgeo import gdal
+import numpy
+
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -17,6 +20,22 @@ def main():
     raster = gdal.OpenEx(raster_path, gdal.OF_RASTER)
     band = raster.GetRasterBand(1)
     LOGGER.debug(band)
+
+    signal_array = numpy.zeros((4000, 4000))
+    kernel_array = numpy.zeros((100, 100))
+    signal_path = 'signal.tif'
+    kernel_path = 'kernel.tif'
+    geoprocessing.numpy_array_to_raster(
+        signal_array, -1, (1, -1), (0, 0), None, signal_path)
+    geoprocessing.numpy_array_to_raster(
+        kernel_array, -1, (1, -1), (0, 0), None, kernel_path)
+
+    target_path = 'convolve2d.tif'
+    LOGGER.debug('starting convolve')
+    geoprocessing.convolve_2d(
+        (signal_path, 1), (kernel_path, 1), target_path,
+        working_dir='convolve_working_dir')
+    LOGGER.debug('all done')
 
 
 if __name__ == '__main__':
