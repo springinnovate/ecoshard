9b207bc72723f0b7d125055edd8c5357a082444c
Rich Sharp <rich@therealspring.org>
2022-05-13 09:52:04 -0700

    re #36 tracking a tracerscript to time/etc

---
+++ convolution_tests.py
@@ -0,0 +1,17 @@
+"""Tracer for running convolution tests."""
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=(
+        '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
+        ' [%(funcName)s:%(lineno)d] %(message)s'))
+
+
+def main():
+    """Entry point."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
