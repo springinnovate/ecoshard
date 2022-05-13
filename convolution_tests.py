aed74323381e00539a6d34451c9825c90b0c6e4b
Rich Sharp <rich@therealspring.org>
2022-05-13 10:50:01 -0700

    log when it starts

--- convolution_tests.py
+++ convolution_tests.py
@@ -19,11 +19,10 @@ def main():
     raster_path = r"D:\ecoshard\fc_stack\fc_stack_hansen_forest_cover_2000-2020_md5_fbb58a.tif"
     raster = gdal.OpenEx(raster_path, gdal.OF_RASTER)
     band = raster.GetRasterBand(1)
-    LOGGER.debug(band)
 
+    LOGGER.debug('create rasters')
     signal_array = numpy.full((10000, 10000), 2)
     kernel_array = numpy.zeros((500, 500))
-
     offset = 50
     for array in [signal_array, kernel_array]:
         array[
@@ -31,6 +30,7 @@ def main():
             array.shape[1]//2-offset:array.shape[1]//2+offset] = 1.0
     signal_path = 'signal.tif'
     kernel_path = 'kernel.tif'
+
     geoprocessing.numpy_array_to_raster(
         signal_array, -1, (1, -1), (0, 0), None, signal_path)
     geoprocessing.numpy_array_to_raster(
