72f5f16b543824be9745d1b3421bfb7ea88a4fb0
Rich Sharp <rich@therealspring.org>
2022-05-23 11:31:42 -0700

    can i make a copy like this?

--- src/ecoshard/geoprocessing/geoprocessing.py
+++ src/ecoshard/geoprocessing/geoprocessing.py
@@ -2554,7 +2554,7 @@ def _calculate_convolve_cache_index(predict_bounds_list):
         index_box = shapely.geometry.box(left, bottom, right, top)
         r_tree.insert(r_tree_index, index_box.bounds, obj=index_box)
         boxes_to_process.append((r_tree_index, index_box))
-    r_tree_copy = rtree.index.Index(r_tree.dumps())
+    r_tree_copy = rtree.index.Index(r_tree)
 
     # break overlapping regions into individual regions but count overlaps
 
