490503168302bf435f2270758852e269b880173b
Richard P Sharp Jr. <richpsharp@gmail.com>
2025-09-14 15:25:05 -0700

    fogot a floodfill

--- src/ecoshard/geoprocessing/routing/__init__.py
+++ src/ecoshard/geoprocessing/routing/__init__.py
@@ -12,7 +12,6 @@ from ecoshard.geoprocessing.routing.routing import (
     extract_strahler_streams_d8,
     calculate_subwatershed_boundary,
     detect_lowest_drain_and_sink,
-    flood_fill,
 )
 
 __all__ = (
